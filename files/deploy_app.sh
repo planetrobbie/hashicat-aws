d56698774150b8a0cd81aabc6cbdb3d6eb13e3a7
Sebastien Braun <sebbraun@gmail.com>
2019-10-02 15:42:25 +0200

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Hello World !!!!! 2019 Hello Cloud Day
+  Welcome to ${PREFIX}'s app. Hello World !!!!! 2019 Hello Cloud Day, almost the end of this section.
   
   </div>
   </body>
